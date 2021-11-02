99295ca37edac844c9c9db7cf36c376ee3cdccbb
headsmanс0de <38921141+headsmanC0de@users.noreply.github.com>
2021-11-02 15:46:55 +0200

    Add reducers

---
+++ react-app-js-redux/src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import './App.css'
+import {useDispatch, useSelector} from "react-redux";
+
+function App() {
+  const dispatch = useDispatch()
+  const cash = useSelector(state => state.cash.cash)
+
+  const addCash = (cash) => {
+    dispatch({type: 'ADD_CASH', payload: cash})
+  }
+
+  const getCash = (cash) => {
+    dispatch({type: 'GET_CASH', payload: cash})
+  }
+
+  return(
+    <div className={'app'}>
+      <div style={{fontSize: '2rem'}}>{cash}</div>
+      <div style={{display: 'flex'}}>
+        <button onClick={() => addCash(Number(prompt()))}>Add Cash</button>
+        <button onClick={() => getCash(Number(prompt()))}>Get Cash</button>
+      </div>
+    </div>
+  )
+}
+
+export default App;
